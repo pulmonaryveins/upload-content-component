3a91df59a1111c80072801f5298c5f1580187e46
pulmonaryveins <vincebryant42@gmail.com>
2026-03-09 10:01:22 +0800

    chore: Updated gitignore

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  transloadit: {
+    key: 'YOUR_TRANSLOADIT_KEY',
+    templateId: 'YOUR_TRANSLOADIT_TEMPLATE_ID',
+  },
+  googleDrive: {
+    apiKey: 'YOUR_GOOGLE_DRIVE_API_KEY',
+    clientId: 'YOUR_GOOGLE_OAUTH_CLIENT_ID',
+    projectNumber: 'YOUR_GOOGLE_PROJECT_NUMBER',
+  },
+};
